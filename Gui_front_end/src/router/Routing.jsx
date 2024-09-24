b370117d04b0b6457c1edb5c15127e90785c2cde
Franderpro <rfrander11@gmail.com>
2024-09-24 15:41:02 -0600

    createstaff

--- Gui_front_end/src/router/Routing.jsx
+++ Gui_front_end/src/router/Routing.jsx
@@ -1,27 +1,9 @@
-<<<<<<< HEAD
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from '../page/LoginGui';
-import HomeInstitutions from '../page/HomeInstitutions';
-
-function Routing() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/login_gui" element={<Login />} />
-        <Route path="/home-institutions" element={<HomeInstitutions />} />
-
-      </Routes>
-    </Router>
-  );
-}
-
-export default Routing;
-=======
 import React from 'react'
 import { BrowserRouter as Router,Routes,Route} from 'react-router-dom'
 import Login from '../page/LoginGui'
 import Home_Gui from '../page/Home_Gui'
+import HomeInstitutions from '../page/HomeInstitutions';
+
 function Routing() {
   return (
     <div>
@@ -29,10 +11,11 @@ function Routing() {
           <Routes>
             <Route path="LoginGUI/" element={<Login />} />
             <Route path="Gui/" element={<Home_Gui />} />
+            <Route path="/home-institutions" element={<HomeInstitutions />} />
+
           </Routes>
         </Router>
     </div>
   )
 }
 export default Routing
->>>>>>> e2fba4107e621ec69022b7688724e936c2f03699
