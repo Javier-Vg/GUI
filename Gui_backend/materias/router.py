38b3237147348cf445ffce12d1b59041f0c0e023
Javier-Vg <jjaviervgg@gmail.com>
2024-09-25 14:41:56 -0600

    correccion

--- Gui_backend/materias/router.py
+++ Gui_backend/materias/router.py
@@ -1,7 +1,7 @@
 from rest_framework.routers import DefaultRouter
-from .views import StudentsViewSet
+from .views import SubjectsViewSet
 
 router_post = DefaultRouter()
 router_post.register(
-    prefix='posts', basename="posts", viewset= StudentsViewSet
+    prefix='posts', basename="posts", viewset= SubjectsViewSet
 )
--- Gui_backend/materias/urls.py
+++ Gui_backend/materias/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import StudentsViewSet
+from .views import SubjectsViewSet
 
 router = DefaultRouter()
-router.register(r'students', StudentsViewSet)
+router.register(r'subjects', SubjectsViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
