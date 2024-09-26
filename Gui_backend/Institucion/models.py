9c9c5991fb50738932793ed7c2c6ec942134297a
Javier-Vg <jjaviervgg@gmail.com>
2024-09-26 09:11:41 -0600

    correcion imgur

--- Gui_backend/Institucion/models.py
+++ Gui_backend/Institucion/models.py
@@ -13,18 +13,14 @@ class Institution(models.Model):  # Cambié el nombre de la clase a singular
     updated_date = models.DateTimeField(auto_now=True)  
     number_phone = models.CharField(max_length=15, blank=False, null=False)  # Cambié a CharField para permitir formatos de teléfono
     email = models.EmailField(validators=[validate_email], blank=False, null=True)
-    imagen = models.ImageField(upload_to='images/', null=True) #permite cargar una imagen y guardarla en la carpeta images/ dentro del directorio de medios.
+    imagen = models.ImageField(null=True) #permite cargar una imagen y guardarla en la carpeta images/ dentro del directorio de medios.
     imagen_url = models.URLField(blank=True, null=True)  #almacenará la URL de la imagen que se sube a Imgur. 
 
-    def save(self, *args, **kwargs):
-        # Llamar al método save para guardar la instancia y acceder a la ruta de la imagen
-        super().save(*args, **kwargs)
-
-        # Subir la imagen a Imgur y guardar la URL
-        if self.imagen and not self.imagen_url:   #Verifica si hay una imagen cargada y si el campo imagen_url está vacío.
-            imagen_url = upload_image_to_imgur(self.imagen.path)  #toma la ruta local de la imagen y la sube a Imgur, devolviendo la URL de la imagen.
-            self.imagen_url = imagen_url #Asigna la URL obtenida al campo imagen_url del modelo.
-            super().save(update_fields=['imagen_url'])
+    def save(self, args, **kwargs):
+    # Subir la imagen a Imgur si no hay imagen_url
+        if self.imagen and not self.imagen_url:
+            imagen_url = upload_image_to_imgur(self.imagen.path)
+            self.imagen_url = imagen_url  # Asignar la URL de Imgur a la instancia
     
     def __str__(self):
         return self.name
