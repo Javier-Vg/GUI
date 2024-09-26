e2ab9ffb9346527d844ee84b66277f25c84f548e
Javier-Vg <jjaviervgg@gmail.com>
2024-09-26 12:11:16 -0600

    Correccion

--- Gui_backend/materias/models.py
+++ Gui_backend/materias/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from Institucion.models import Institution  # Importa el modelo de Institution
-from teachers.models import teachers  # Importa el modelo de Institution
+from staff.models import staff  # Importa el modelo de Institution
 from groups.models import groups  # Importa el modelo de Institution
 
 # Create your models here.
 class subjects(models.Model):  # materia = subject
     subject_name =  models.CharField(max_length=100, blank=False, null=False)
     educational_level =  models.CharField(max_length=100, blank=False, null=False)
-    teacher = models.ForeignKey(teachers, on_delete=models.CASCADE)
+    teacher = models.ForeignKey(staff, on_delete=models.CASCADE)
     institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
     group = models.ForeignKey(groups, on_delete=models.CASCADE)
--- Gui_backend/staff/router.py
+++ Gui_backend/staff/router.py
@@ -1,7 +1,7 @@
 from rest_framework.routers import DefaultRouter
-from .views import TeachersViewSet
+from .views import StaffViewSet
 
 router_post = DefaultRouter()
 router_post.register(
-    prefix='posts', basename="posts", viewset= TeachersViewSet
+    prefix='posts', basename="posts", viewset= StaffViewSet
 )
