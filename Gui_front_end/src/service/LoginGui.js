ecd3e4dac96cbdb076c5bc2e6fb57d74e394707e
Javier-Vg <jjaviervgg@gmail.com>
2024-09-27 08:26:39 -0600

    commit

--- Gui_front_end/src/service/LoginGui.js
+++ Gui_front_end/src/service/LoginGui.js
@@ -53,3 +53,42 @@ export const postInstitutions = async (name, address, estado, subscriptionType, 
     }
   };
   
+
+
+
+
+  export const getStaff = async () => {
+    try {
+      const response = await axios.get('http://localhost:8000/api/staff/staff/');
+      return response.data
+    } catch (error) {
+      console.error('Error fetching user data:', error);
+      throw error;
+    }
+  };
+  
+export const postStaff = async (name, address, estado, subscriptionType, phoneNumber, email,date,Image) => {
+    try {
+      const response = await axios.post("http://localhost:8000/api/staff/staff/", {
+        name: name,
+        last_name: address,
+        identification_number : estado,
+        birthdate_date: subscriptionType,
+        direction: phoneNumber,
+        phone_number: email,
+        email: date,
+        employment_status: date,
+        position: date,
+        salary: date,
+        imagen: date,
+        contract: date,
+        subjects: date,
+        school_subjects:Image
+      });
+      return response.data;
+    } catch (error) {
+      console.error("Error haciendo la solicitud:", error);
+      throw error;
+    }
+  };
+  
