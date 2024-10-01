171d32d89c62a479188b46d4841e06ffaba4fde1
23dereek <168207899+DerekNoguera@users.noreply.github.com>
2024-10-01 10:19:02 -0600

    x

--- Gui_backend/students/models.py
+++ Gui_backend/students/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from Institucion.models import Institution  # Importa el modelo de Institution
 from groups.models import group  # Importa el modelo de Institution
-from .utils import upload_image_to_imgur
 
 # Create your models here.
 class students(models.Model):
@@ -31,18 +30,7 @@ class students(models.Model):
     contact_information = models.CharField(max_length=100, blank=False, null=False)
     institution =  models.ForeignKey(Institution, on_delete=models.CASCADE)
     group =  models.ForeignKey(group, on_delete=models.CASCADE)
-    imagen = models.ImageField(upload_to='images/', null=True) #permite cargar una imagen y guardarla en la carpeta images/ dentro del directorio de medios.
     imagen_url = models.URLField(blank=True, null=True)  #almacenará la URL de la imagen que se sube a Imgur. 
-    
-    def save(self, *args, **kwargs):
-        # Llamar al método save para guardar la instancia y acceder a la ruta de la imagen
-        super().save(*args, **kwargs)
-
-        # Subir la imagen a Imgur y guardar la URL
-        if self.imagen and not self.imagen_url:   #Verifica si hay una imagen cargada y si el campo imagen_url está vacío.
-            imagen_url = upload_image_to_imgur(self.imagen.path)  #toma la ruta local de la imagen y la sube a Imgur, devolviendo la URL de la imagen.
-            self.imagen_url = imagen_url #Asigna la URL obtenida al campo imagen_url del modelo.
-            super().save(update_fields=['imagen_url'])
     
     def __str__(self):
         return self.name
