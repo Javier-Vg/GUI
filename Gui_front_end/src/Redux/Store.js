f838fcb895d8d6227793d5c6d79cd4659f08ce9f
Javier Vargas Guzman <Javier-Vg>
2024-10-08 01:37:51 -0600

    primer avance redux

---
+++ Gui_front_end/src/Redux/Store.js
@@ -0,0 +1,17 @@
+//quede aca (●'◡'●)cons
+// import { configureStore, combineReducers } from "@reduxjs/toolkit";
+// import {thunk} from 'redux-thunk';
+// import logger from "redux-logger";
+// import { Reducer } from "./Reducer";
+
+// const rootreducer = combineReducers(
+//   { user: Reducer 
+//     //Aca añadirias otro reducer, 
+//   });
+
+// const Store = configureStore({
+//   reducer: rootreducer,
+//   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk, logger),
+// });
+
+// export default Store;
