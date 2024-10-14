cccd93676dae22c6ac135a1c676020e3450e52d6
23dereek <168207899+DerekNoguera@users.noreply.github.com>
2024-10-14 11:31:08 -0600

    x

--- Gui_front_end/src/Redux/Store.js
+++ Gui_front_end/src/Redux/Store.js
@@ -65,18 +65,24 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import { configureStore } from '@reduxjs/toolkit';
 import { thunk } from 'redux-thunk'; 
+
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage'; // Usará localStorage por defecto
-import institutionReducer from '../Redux/Slices/SliceInstitution';
+import SliceInfInstitution from '../Redux/Slices/SliceInfInstitution';
+
+import institutionReducer from '../Redux/Slices/SliceInstitution'; //Se importan con un nombre cualquiera, no esta definido en el archico.
 import staffReducer from '../Redux/Slices/SliceStaff';
 import groupReducer from '../Redux/Slices/SliceGroup';
+
 import studentReducer from '../Redux/Slices/SliceStudent';
 import subjectReducer from '../Redux/Slices/SliceSubjects';
 import scheduleReducer from '../Redux/Slices/SliceSchedule';
+
 import taskReducer from '../Redux/Slices/SliceTask';
 import loginReducer from '../Redux/Slices/SliceLogin';
-import SliceInfInstitution from '../Redux/Slices/SliceInfInstitution';
+import contractReducer from '../Redux/Slices/SliceContract';
 
+import eventReducer from '../Redux/Slices/SliceEvent';
 const isDev = process.env.NODE_ENV === 'development';
 
 // Configuración de persistencia
