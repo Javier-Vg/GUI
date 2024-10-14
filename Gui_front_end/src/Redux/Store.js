a49de695d6a241d0304e88fb9aef915382c20731
23dereek <168207899+DerekNoguera@users.noreply.github.com>
2024-10-14 10:12:13 -0600

    Arreglo de errores

--- Gui_front_end/src/Redux/Store.js
+++ Gui_front_end/src/Redux/Store.js
@@ -14,10 +14,60 @@
 //   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk, logger),
 // });
 // //quede aca (●'◡'●)cons
+
+
+
+
+// import { configureStore } from '@reduxjs/toolkit';
+// import {thunk} from 'redux-thunk'; // Asegúrate de tener esto
+// // import logger from 'redux-logger'; // Asegúrate de tener esto
+// import institutionReducer from '../Redux/Slices/SliceInstitution'; //Se importan con un nombre cualquiera, no esta definido en el archico.
+// import staffReducer from '../Redux/Slices/SliceStaff';
+// import groupReducer from '../Redux/Slices/SliceGroup';
+// import studentReducer from '../Redux/Slices/SliceStudent';
+// import subjectReducer from '../Redux/Slices/SliceSubjects';
+// import scheduleReducer from '../Redux/Slices/SliceSchedule';
+// import taskReducer from '../Redux/Slices/SliceTask';
+// import loginReducer from '../Redux/Slices/SliceLogin';
+// import contractReducer from '../Redux/Slices/SliceContract';
+// import eventReducer from '../Redux/Slices/SliceEvent';
+// const isDev = process.env.NODE_ENV === 'development';
+
+// const Store = configureStore({
+//   reducer: {
+//     institutions: institutionReducer,
+//     staff: staffReducer,
+//     group: groupReducer,
+//     student: studentReducer,
+//     subject: subjectReducer,
+//     schedule: scheduleReducer,
+//     task: taskReducer,
+//     login: loginReducer,
+//     contract: contractReducer,
+//     event: eventReducer
+
+//   },
+//   middleware: (getDefaultMiddleware) => {
+//     const middlewares = getDefaultMiddleware().concat(thunk);
+
+//     // if (isDev) {
+//     //   middlewares.push(logger); // Solo agregar logger en desarrollo
+//     // }
+
+//     return middlewares;
+//   },
+//   devTools: isDev, // Habilitar DevTools solo en desarrollo
+  
+// });
+
+// export default Store;
+
+import { combineReducers } from '@reduxjs/toolkit';
 import { configureStore } from '@reduxjs/toolkit';
-import {thunk} from 'redux-thunk'; // Asegúrate de tener esto
-// import logger from 'redux-logger'; // Asegúrate de tener esto
-import institutionReducer from '../Redux/Slices/SliceInstitution'; //Se importan con un nombre cualquiera, no esta definido en el archico.
+import { thunk } from 'redux-thunk'; 
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // Usará localStorage por defecto
+import institutionReducer from '../Redux/Slices/SliceInstitution';
 import staffReducer from '../Redux/Slices/SliceStaff';
 import groupReducer from '../Redux/Slices/SliceGroup';
 import studentReducer from '../Redux/Slices/SliceStudent';
@@ -25,12 +75,18 @@ import subjectReducer from '../Redux/Slices/SliceSubjects';
 import scheduleReducer from '../Redux/Slices/SliceSchedule';
 import taskReducer from '../Redux/Slices/SliceTask';
 import loginReducer from '../Redux/Slices/SliceLogin';
-import contractReducer from '../Redux/Slices/SliceContract';
-import eventReducer from '../Redux/Slices/SliceEvent';
+import SliceInfInstitution from '../Redux/Slices/SliceInfInstitution';
+
 const isDev = process.env.NODE_ENV === 'development';
 
-const Store = configureStore({
-  reducer: {
+// Configuración de persistencia
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+// Combinar todos los reducers en uno solo
+const rootReducer = combineReducers({
     institutions: institutionReducer,
     staff: staffReducer,
     group: groupReducer,
@@ -40,21 +96,22 @@ const Store = configureStore({
     task: taskReducer,
     login: loginReducer,
     contract: contractReducer,
-    event: eventReducer
-
-  },
-  middleware: (getDefaultMiddleware) => {
-    const middlewares = getDefaultMiddleware().concat(thunk);
-
-    // if (isDev) {
-    //   middlewares.push(logger); // Solo agregar logger en desarrollo
-    // }
-
-    return middlewares;
-  },
-  devTools: isDev, // Habilitar DevTools solo en desarrollo
-  
+    event: eventReducer,
+    infInstitution: SliceInfInstitution,
 });
 
-export default Store;
+// Aplicar persistencia al rootReducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
+const Store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware({
+      serializableCheck: false, // Deshabilita la verificación de serialización
+    }).concat(thunk);
+  },
+  devTools: isDev,
+});
+
+export const persistor = persistStore(Store);
+export default Store;
--- Gui_front_end/src/main.jsx
+++ Gui_front_end/src/main.jsx
@@ -1,19 +1,36 @@
-// src/index.js
+// // src/index.js
+// import React from 'react';
+// import ReactDOM from 'react-dom/client'; // Importa createRoot desde react-dom/client
+// import { Provider } from 'react-redux';
+// import App from './App';
+// import Store from './Redux/Store';
+
+// // Obtén el elemento root del DOM
+// const rootElement = document.getElementById('root');
+
+// // Crea el root usando createRoot
+// const root = ReactDOM.createRoot(rootElement);
+
+// // Renderiza la aplicación con Provider y el store
+// root.render(
+//   <Provider store={Store}>
+//     <App />
+//   </Provider>
+// );
+
 import React from 'react';
-import ReactDOM from 'react-dom/client'; // Importa createRoot desde react-dom/client
+import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App';
-import Store from './Redux/Store';
+import Store, { persistor } from './Redux/Store';
+import { PersistGate } from 'redux-persist/integration/react';
 
-// Obtén el elemento root del DOM
-const rootElement = document.getElementById('root');
-
-// Crea el root usando createRoot
-const root = ReactDOM.createRoot(rootElement);
-
-// Renderiza la aplicación con Provider y el store
-root.render(
-  <Provider store={Store}>
-    <App />
-  </Provider>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={Store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
 );
